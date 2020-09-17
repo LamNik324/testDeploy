2be0411f2d0552389173c68660c810967a1cded1
LamNik324 <89999809989@yandex.ru>
2020-09-17 14:42:46 +0300

    f

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import User from "./models/user.js";
+// import User from "./models/user.js";
 
 dotenv.config();
 const app = express();
@@ -29,47 +29,6 @@ app.get('/test', (req, res) => {
   res.send('Im am alive')
 })
 
-app.get("/api", async (req, res) => {
-  const todos = await Todo.find();
-  setTimeout(() => {
-    res.send(todos);
-  }, 1000)
-  // res.send(todos)
-});
-
-app.post("/api/create", async (req, res) => {
-  const { todo } = req.body;
-  const todoId = await Todo.create({
-    title: todo,
-    completed: false,
-    createdAt: new Date().toLocaleTimeString(),
-  })
-  res.json(todoId)
-});
-
-app.post('/api/delete/:id', async (req, res) => {
-  const {id} = req.body;
-  await Todo.deleteOne({_id: id});
-  res.end();
-})
-
-app.post('/api/update/:id', async (req, res) => {
-  const {id} = req.params
-  const {title} = req.body
-  const todo = await Todo.findOne({_id: id})
-  todo.title = title
-  todo.save()
-  res.send(todo)
-})
-
-app.post('/api/done/:id', async (req, res) => {
-  const {id} = req.params;
-  console.log(id);
-  const todoCompleted = await Todo.findById(id)
-  todoCompleted.completed = !todoCompleted.completed
-  todoCompleted.save()
-  res.send(todoCompleted)
-})
 
 const port = process.env.PORT ?? 4000;
 
